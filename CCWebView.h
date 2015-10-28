a1a91a196326024812f1df06d37ecdde544524ac
raven <raven.zhang@kanche.com>
2015-10-28 12:36:40 +0800

    add CCWebview

---
+++ CCWebView.h
@@ -0,0 +1,13 @@
+//
+//  CCWebView.h
+//  
+//
+//  Created by Raven on 15/10/27.
+//  Copyright © 2015年 Kanche.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCWebView : UIView
+
+@end
---
+++ CCWebView.m
@@ -0,0 +1,21 @@
+//
+//  CCWebView.m
+//  
+//
+//  Created by Raven on 15/10/27.
+//  Copyright © 2015年 Kanche.com. All rights reserved.
+//
+
+#import "CCWebView.h"
+
+@implementation CCWebView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
